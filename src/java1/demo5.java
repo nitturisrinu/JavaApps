e1d0faa6e0ec411177bc6597db8b655047aca403
nitturisrinu <97295256+nitturisrinu@users.noreply.github.com>
2022-01-17 01:13:03 +0530

    newly changed programm

--- src/java1/demo5.java
+++ src/java1/demo5.java
@@ -3,5 +3,16 @@ package java1;
 public class demo5 {
 	public static void main(String[] args) {
 		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		
 	}
 }
