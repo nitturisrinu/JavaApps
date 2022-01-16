237427eb177f899f899316b0aea447689ac807fd
nitturisrinu <nsrinu540@gmail.com>
2022-01-17 01:27:40 +0530

    change

---
+++ src/java1/Demo6.java
@@ -0,0 +1,16 @@
+package java1;
+
+public class Demo6 {
+
+	public static void main(String[] args) {
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("hiiiiiiii");
+		System.out.println("h");
+	
+		
+	}
+}
